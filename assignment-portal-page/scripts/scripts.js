38f631bb7cd0eca18d4326dce9a6d4160c2c23f7
Henryohia <henryohia197@gmail.com>
2023-01-10 03:40:19 +0100

    portal page added

---
+++ assignment-portal-page/scripts/scripts.js
@@ -0,0 +1,5 @@
+let text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
+
+// const date = new Date(document.lastModified);
+// document.getElementById("demo").innerHTML = date;
