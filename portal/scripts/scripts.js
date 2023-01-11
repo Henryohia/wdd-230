431cc5e1d532ec0325e4f85368176697a026728a
Henryohia <henryohia197@gmail.com>
2023-01-11 19:39:37 +0100

    portal page added

--- portal/scripts/scripts.js
+++ portal/scripts/scripts.js
@@ -1,5 +1,9 @@
 let text = document.lastModified;
 document.getElementById("demo").innerHTML = text;
 
+let result = text.fontcolor("blue");
+document.getElementById("demo").innerHTML = result;
+
+
 // const date = new Date(document.lastModified);
 // document.getElementById("demo").innerHTML = date;
