9d2303cefddc78ac9cdc55029c1773878528ef19
Henryohia <henryohia197@gmail.com>
2023-03-21 07:30:56 +0100

    portal page updated

--- portal/scripts/scripts.js
+++ portal/scripts/scripts.js
@@ -1,9 +1,14 @@
-let text = document.lastModified;
-document.getElementById("demo").innerHTML = text;
+// let text = document.lastModified;
+// document.getElementById("demo").innerHTML = text;
 
-let result = text.fontcolor("blue");
-document.getElementById("demo").innerHTML = result;
-
+// let result = text.fontcolor("blue");
+// document.getElementById("demo").innerHTML = result;
 
 // const date = new Date(document.lastModified);
 // document.getElementById("demo").innerHTML = date;
+
+// ****************************************************************
+let text = document.lastModified;
+document.querySelector(
+  "#lastModified"
+).textContent = `Last Updated: ${document.lastModified}`;
